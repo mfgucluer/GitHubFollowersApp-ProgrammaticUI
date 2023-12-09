61a977eaf143d39bc6aece3ac9f0cba681404189
Mustafa Fatih GUCLUER <fatihgucluer@hotmail.com>
2023-12-09 17:19:00 +0300

    Deleting Storyboard operations are completed

--- GHFollowersProgrammaticUI/SceneDelegate.swift
+++ GHFollowersProgrammaticUI/SceneDelegate.swift
@@ -9,21 +9,20 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-    var window: UIWindow?
+    var window: UIWindow? //Buradaki window eskiden appDelegate icindeydi... Simdi sceneDelegate icinde. SceneDelegate tum UI lari configure ediyor boylece
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let windowSceneX = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowSceneX.coordinateSpace.bounds) //ekrani kaplamasi icin
+        window?.windowScene = windowSceneX
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+      
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
--- GHFollowersProgrammaticUI/ViewController.swift
+++ GHFollowersProgrammaticUI/ViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+       
     }
-
+    
+    
+    
+    
 
 }
-
